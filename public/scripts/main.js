0b2bc0b3a7be084609f81abdd905b084f38076e8
Luziane Freitas <freitas.lu@outlook.com>
2021-06-24 17:10:14 -0300

    finished: modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,19 @@
 // Todos os detalhes do front ficaram aqui!!!!!!!!!!!!
-import Modal from './modal.js'
+import Modal from './modal.js';
 
-const modal = Modal()
+const modal = Modal();
+
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
 
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
 
-    button.addEventListener('click', event => {
-        // Abrir modal
-        modal.open()
-    })
+    button.addEventListener('click', handleClick)
 });
 
 
@@ -20,8 +22,18 @@ const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
 
-    button.addEventListener('click', event => {
-        // fechar modal
-        modal.open()
-    })
+    button.addEventListener('click', (event) =>  handleClick(event,false))
 });
+
+function handleClick(event, check = true) {
+    event.preventDefault();
+    const text = check ? 'Marcar como lida' : 'Excluir';
+
+    modalTitle.innerHTML = `${text} esta pergunta`;
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} essa pergunta?`;
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`;
+    check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+    // Abrir modal
+    modal.open();
+}
