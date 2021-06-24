e8229a4dd4e1a15fb675acc279f38d59097646b5
Luziane Freitas <freitas.lu@outlook.com>
2021-06-24 17:37:33 -0300

    add: params modal

--- src/route.js
+++ src/route.js
@@ -6,4 +6,7 @@ route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 
+// Formato que o formulário de dentro do modal tem que passar
+// route.post('/room/:room/:question/:action', (req, res) => res.render())
+
 module.exports = route
