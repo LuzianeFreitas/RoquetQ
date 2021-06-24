85d176a47b676ac2382328428d37d7890342251c
Luziane Freitas <freitas.lu@outlook.com>
2021-06-24 20:54:56 -0300

    add: function roomcontroller

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,8 +1,28 @@
+const Database = require('../db/config');
+
 module.exports = {
-    create(req, res) {
-        let roomId = 123456
+    async create(req, res) {
+        const db = await Database();
+        const pass = req.body.password
+        let roomId
+
+        for(var i = 0; i < 6; i++) {
+            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+            roomId += Math.floor(Math.random() * 10).toString()
+        }
+
+        // console.log(parseInt(roomId));
+
+        await db.run(`INSERT INTO rooms (
+            id,
+            pass
+        ) VALUES (
+            ${parseInt(roomId)},
+            ${pass}
+        )`)
         
-        
+        await db.close()
+
         res.redirect(`/room/${roomId}`)
     }
 }
