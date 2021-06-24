539060f06143722434e84ebd84f508a43c2c670c
Luziane Freitas <freitas.lu@outlook.com>
2021-06-24 19:46:28 -0300

    add: routes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -34,7 +34,7 @@ function handleClick(event, check = true) {
 
 
     const form = document.querySelector('.modal form')
-    form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+    form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML = `${text} esta pergunta`;
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} essa pergunta?`;
--- src/route.js
+++ src/route.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const QuestionController = require('./controllers/QuestionController');
+const RoomController = require('./controllers/RoomController');
 
 const route = express.Router();
 
-route.get('/', (req, res) => res.render('index'))
-route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}));
+route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}));
+
+route.get('/room/:rom', (req, res) => res.render('room'));
 
 // Formato que o formulário de dentro do modal tem que passar
-route.post('/room/:room/:question/:action', QuestionController.index)
+route.post('/question/:room/:question/:action', QuestionController.index);
+route.post('/create-room', RoomController.create);
 
 module.exports = route
