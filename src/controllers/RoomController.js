c15e220fc5a74dc0053fcb47353b487955d83990
Luziane Freitas <freitas.lu@outlook.com>
2021-06-25 17:27:04 -0300

    update: open RoomController

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -38,8 +38,13 @@ module.exports = {
         res.redirect(`/room/${roomId}`)
     },
 
-    open(req, res){
+    async open(req, res){
+        const db = await Database();
         const roomId = req.params.room
-        res.render('room', { roomId: roomId })
+        
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+
+        res.render('room', { roomId: roomId,  questions: questions, questionsRead: questionsRead})
     }
 }
