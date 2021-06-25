4018f8a57b4fc1cedb10641480c50d7e10e9d5e7
Luziane Freitas <freitas.lu@outlook.com>
2021-06-25 16:06:00 -0300

    add: dinamic code class

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -5,21 +5,33 @@ module.exports = {
         const db = await Database();
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(var i = 0; i < 6; i++) {
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        while(isRoom) {
+
+            /**Gera o número da sala */
+            for(var i = 0; i < 6; i++) {
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            /**Verifica se o número existe */
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+            /**some verifica a condição passada e se ela existir retorna um true */
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            if(!roomsExistIds){
+                    /**Insere a sala no banco */
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
-
-        // console.log(parseInt(roomId));
-
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
         
         await db.close()
 
