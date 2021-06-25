30e6208105e7d945a382b2162a9e10e8a3e0729d
Luziane Freitas <freitas.lu@outlook.com>
2021-06-25 18:06:38 -0300

    update: index QuestionController

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,14 +1,32 @@
 const Database = require('../db/config');
 
 module.exports = {
-    index(req, res){
+    async index(req, res){
+        const db = await Database();
         const roomId = req.params.room;
         const questionId = req.params.question;
         const action = req.params.action;
         const password = req.body.password;
 
-        console.log(`room = ${roomId}, question = ${questionId}, action = ${action}, password = ${password}`);
+        /**Verificar se a senha está correta */
+        /**db.get() -> trás apenas um dado do banco */
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`);
+        
+        if(verifyRoom.pass == password){
+            if(action == 'delete') {
 
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`);
+
+            } else if(action == 'check') {
+
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`);
+            }
+            res.redirect(`/room/${roomId}`);
+        } else {
+            res.render(`passincorrect`, { roomId: roomId });
+        }
+
+        
     },
 
     async create(req, res){
