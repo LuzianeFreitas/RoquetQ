5b9b077f23dff8d156c3ee0876b526235acb8b61
Luziane Freitas <freitas.lu@outlook.com>
2021-06-26 20:44:06 -0300

    finished: dark-mode

--- public/scripts/dark-mode.js
+++ public/scripts/dark-mode.js
@@ -4,27 +4,55 @@ const logo = document.querySelector('header a img');
 const btn = document.querySelector('a.button');
 const iconBtn = document.querySelector('a.button img');
 const bg = document.querySelector('div#bg');
-const colorInputFont = document.querySelector('form input')
+const colorInputFont = document.querySelector('form input');
+const colorTextAreaFont = document.querySelector('form textarea');
+const btnCopy = document.querySelector('div .buttons .button.outlined');
+const btnCopyImg = document.querySelector('div .buttons .button.outlined img')
+const noQuestion = document.querySelector('.no-questions img');
+
+const idHome = document.getElementById('home');
+const idRoom = document.getElementById('room');
 
 checkBox.addEventListener('click', function(){
     html.classList.toggle('dark-mode');
-    
-    if(html.classList.contains('dark-mode')) {
-        logo.src = "/images/logo-dark.svg";
-        colorInputFont.classList.toggle('dark-mode-color');
-        if(btn != null) {
-            iconBtn.src = "/images/users-dark.svg";
-            btn.classList.toggle('dark-mode-btn');
-        }        
-        bg.classList.toggle('dark-mode-img')
 
-    }else{
-        logo.src = "/images/logo.svg"
-        colorInputFont.classList.remove('dark-mode-color');
-        if(btn != null) {
-            iconBtn.src = "/images/users.svg";
-            btn.classList.remove('dark-mode-btn');
+    if(idHome) {
+        if(html.classList.contains('dark-mode')) {
+            logo.src = "/images/logo-dark.svg";
+            colorInputFont.classList.toggle('dark-mode-color');
+            if(btn != null) {
+                iconBtn.src = "/images/users-dark.svg";
+                btn.classList.toggle('dark-mode-btn');
+            }        
+            bg.classList.toggle('dark-mode-img')
+    
+        }else{
+            logo.src = "/images/logo.svg"
+            colorInputFont.classList.remove('dark-mode-color');
+            if(btn != null) {
+                iconBtn.src = "/images/users.svg";
+                btn.classList.remove('dark-mode-btn');
+            }
+            
         }
-        
+
+    } else if(idRoom) {
+        if(html.classList.contains('dark-mode')) {
+            logo.src = "/images/logo-dark.svg";
+            btnCopy.classList.toggle('dark-mode-copy');
+            btnCopyImg.src = "/images/copy-dark.svg"
+            colorTextAreaFont.classList.toggle('dark-mode-color');
+            if(noQuestion != null)
+                noQuestion.src = "/images/no-questions-dark.svg";
+            
+        }else{
+            logo.src = "/images/logo.svg";
+            btnCopy.classList.remove('dark-mode-copy');
+            btnCopyImg.src = "/images/copy.svg"
+            colorTextAreaFont.classList.remove('dark-mode-color');
+            if(noQuestion != null)
+                noQuestion.src = "/images/no-questions.svg";
+
+        }
     }
 });
